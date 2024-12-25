9705aad0b835064dabcddd360199c373f70326c6
HAWKSTER7337 <ahawk2255@gmail.com>
2024-12-25 10:26:49 -0500

    Added way to search for people
    you can now search for people and added a terminal interface so the code can now be used without changing it each time

--- CinemaSeer/Endpoints/SearchMovieInformation.cs
+++ CinemaSeer/Endpoints/SearchMovieInformation.cs
@@ -2,6 +2,13 @@ using CinemaSeer.JsonFormats;
 
 namespace CinemaSeer.Endpoints;
 
+/// <summary>
+/// Represents a class used to search for movie information by connecting to the TMDB API endpoint.
+/// </summary>
+/// <remarks>
+/// This class provides various filter options, such as query, page, language, region, release year, and primary release year.
+/// It populates query parameters to be used for API calls and clears them when needed.
+/// </remarks>
 public class SearchMovieInformation : VideoEndpoint<Movie>
 {
     // Filter Options 
--- CinemaSeer/Program.cs
+++ CinemaSeer/Program.cs
@@ -8,24 +8,76 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        var movieSearch = new SearchMovieInformation();
-        movieSearch.Query = "Lord of The Rings ";
-        var response = await movieSearch.GetInformationAsync();
+        await TestingLoop();
+    }
 
-        var tvShowSearch = new SearchTvShowInformation();
-        tvShowSearch.Query = "Stranger Things";
-        var response2 = await tvShowSearch.GetInformationAsync();
-
-        var fullList = new List<IMedia>();
-        fullList.AddRange(response2.Results);
-        
-        // Getting the places to watch the movies
-        var watchProviders = new GetWatchProviders();
-        foreach (var video in fullList)
+    private static async Task TestingLoop()
+    {
+        var usersChoice = -1;
+        while (usersChoice != 4)
         {
-            watchProviders.mediaItem = video.GetInfo();
-            var placeToWatch = await watchProviders.GetInformationAsync();
-            Console.WriteLine(placeToWatch.GetUSPlacesToStream());
+            Console.WriteLine("\n\n\n\n\n");
+            Console.WriteLine(
+                "Please Select an option:\n" +
+                "1. Search Movie Information\n" +
+                "2. Search Tv Show Information\n" +
+                "3. Search Person Information\n" +
+                "4. Exit"
+                );
+            usersChoice = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n\n\n\n\n");
+
+            if (usersChoice == 1)
+            {
+                var searchMovie = new SearchMovieInformation();
+                Console.WriteLine("What is the Movie you are looking to find?");
+                searchMovie.Query = Console.ReadLine();
+                var movieResults = await searchMovie.GetInformationAsync();
+                var watchProviders = new GetWatchProviders();
+                foreach (var movie in movieResults.Results)
+                {
+                    watchProviders.mediaItem = movie.GetInfo();
+                    var placesToWatch = await watchProviders.GetInformationAsync();
+                    Console.WriteLine(movie.ToBasicDataString());
+                    Console.WriteLine(placesToWatch.GetUSPlacesToStream());
+                }
+            }
+
+            else if (usersChoice == 2)
+            {
+                var SeachTvShows = new SearchTvShowInformation();
+                Console.WriteLine("What is the Tv Show you are looking to find?");
+                SeachTvShows.Query = Console.ReadLine();
+                var tvShowSearchResults = await SeachTvShows.GetInformationAsync();
+                var watchProviders = new GetWatchProviders();
+                foreach (var tvShow in tvShowSearchResults.Results)
+                {
+                    watchProviders.mediaItem = tvShow.GetInfo();
+                    var placesToWatch = await watchProviders.GetInformationAsync();
+                    Console.WriteLine(tvShow.ToBasicDataString());
+                    Console.WriteLine(placesToWatch.GetUSPlacesToStream());
+                }
+            }
+
+            else if (usersChoice == 3)
+            {
+                var personInfoSearch = new SearchPersonInformation();
+                Console.WriteLine("Who is the actor you are looking to find?");
+                personInfoSearch.Query = Console.ReadLine();
+                var personSearchResults = await personInfoSearch.GetInformationAsync();
+                personSearchResults.Results.ForEach(person => Console.WriteLine(person.ToBasicDataString()));
+            }
+            
+            else if (usersChoice == 4)
+            {
+                Console.WriteLine("Exiting...");
+                break;
+            }
+
+            else
+            {
+                Console.WriteLine("Not a proper input please try again...");
+            }
         }
     }
 }
